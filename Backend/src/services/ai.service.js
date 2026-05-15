0be5bd232e3092da59942faf8724a6019a4ffdb3
Dev-13552 <devrathour110@gmail.com>
2026-05-15 21:42:08 +0530

    Made changes to ai.service.js  and add postinstall to package.json

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -121,7 +121,7 @@ async function generatePdfFromHtml(htmlContent) {
   const browser = await puppeteer.launch({
   executablePath: puppeteer.executablePath(),
   headless: true,
-  args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  args: ["--no-sandbox", '--disable-setuid-sandbox', '--disable-dev-shm-usage']
 });
   const page = await browser.newPage();
   await page.setContent(htmlContent, { waitUntil: "networkidle0" });
