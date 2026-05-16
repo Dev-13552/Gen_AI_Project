bf58cabbadd1cac51c3622531e04429568110577
Dev-13552 <devrathour110@gmail.com>
2026-05-16 11:29:44 +0530

    add resend to send emails

--- Backend/src/config/email.js
+++ Backend/src/config/email.js
@@ -1,37 +1,57 @@
 const nodemailer = require("nodemailer");
+const { Resend } = require('resend');
 require("dotenv").config();
 
- const verifyEmail = async (token, email) => {
-  let mailTransporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,           // or 465
-    secure: false,       // true if using port 465
-    family: 4,
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASS,
-    },
-  });
+//  const verifyEmail = async (token, email) => {
+//   // let mailTransporter = nodemailer.createTransport({
 
-  let mailDetails = {
-    from: process.env.MAIL_USER,
-    to: email,
-    subject: "Email Verification",
-    text: `Hi! There, You have recently visited our website and entered your email.
-            Please follow the given link to verify your email
-            https://gen-ai-project-tau.vercel.app/verify/${token}
-            Thanks`,
-  };
+//   //   host: "smtp.gmail.com",
+//   //   port: 587,           // or 465
+//   //   secure: false,       // true if using port 465
+//   //   family: 4,
+//   //   connectionTimeout: 10000,
+//   //   greetingTimeout: 10000,
+//   //   auth: {
+//   //     user: process.env.MAIL_USER,
+//   //     pass: process.env.MAIL_PASS,
+//   //   },
+//   // });
 
-  mailTransporter.sendMail(mailDetails, function (err, data) {
-    if (err) {
-      console.log("Error Occurs", err);
-    } else {
-      console.log("Email sent successfully");
-    }
-  });
+//   // let mailDetails = {
+//   //   from: process.env.MAIL_USER,
+//   //   to: email,
+//   //   subject: "Email Verification",
+//   //   text: `Hi! There, You have recently visited our website and entered your email.
+//   //           Please follow the given link to verify your email
+//   //           https://gen-ai-project-tau.vercel.app/verify/${token}
+//   //           Thanks`,
+//   // };
+
+//   // mailTransporter.sendMail(mailDetails, function (err, data) {
+//   //   if (err) {
+//   //     console.log("Error Occurs", err);
+//   //   } else {
+//   //     console.log("Email sent successfully");
+//   //   }
+//   // });
+// };
+
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const verifyEmail = async (token, email) => {
+  try {
+    await resend.emails.send({
+      from: 'uniq5344@gmail.com',
+      to: email,
+      subject: 'Email Verification',
+      text: `Please verify your email: https://gen-ai-project-tau.vercel.app/verify/${token}`,
+    });
+    console.log("Email sent successfully");
+  } catch (err) {
+    console.log("Error Occurs", err);
+    throw err;
+  }
 };
 
 module.exports = {verifyEmail}
